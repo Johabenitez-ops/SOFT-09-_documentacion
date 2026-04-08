c2d613674943fed61a23357e098360e65905e834
Johabenitez-ops <joha16benitez@gmail.com>
2026-04-07 22:55:59 -0600

    Mi primer cambio visible

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,7 +1,7 @@
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("¡Hola,  mundo desde Java!");
-        saludar("Joha nna");
+        saludar("nombre");
         sumar(5, 3);
     }
     public static void saludar(String nombre) {
@@ -11,4 +11,5 @@ public class HolaMundo {
         int resultado = a + b;
         System.out.println("La suma de " + a + " + " + b + " es: " + resultado);
         }
+       // Este es un comentario de prueba.
     }
